cdc52208805d8ca58c862d6781da20e81491112e
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-01-02 15:30:12 -0600

    UPDATE | Cleaning Personal Projects. Happy coding :)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,52 +1,52 @@
-import React, { useRef, useState } from 'react';
-import '../assets/styles/Contact.scss';
-// import emailjs from '@emailjs/browser';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import TextField from '@mui/material/TextField';
+import { FormEvent, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import SendIcon from "@mui/icons-material/Send";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import "../assets/styles/Contact.scss";
 
 function Contact() {
-
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
   const [nameError, setNameError] = useState<boolean>(false);
   const [emailError, setEmailError] = useState<boolean>(false);
   const [messageError, setMessageError] = useState<boolean>(false);
 
-  const form = useRef();
+  const formRef = useRef<HTMLFormElement>(null);
 
-  const sendEmail = (e: any) => {
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    setNameError(name === '');
-    setEmailError(email === '');
-    setMessageError(message === '');
+    setNameError(name === "");
+    setEmailError(email === "");
+    setMessageError(message === "");
 
     /* Uncomment below if you want to enable the emailJS */
 
-    // if (name !== '' && email !== '' && message !== '') {
-    //   var templateParams = {
-    //     name: name,
-    //     email: email,
-    //     message: message
-    //   };
+    if (name !== "" && email !== "" && message !== "") {
+      var templateParams = {
+        name: name,
+        email: email,
+        message: message,
+      };
 
-    //   console.log(templateParams);
-    //   emailjs.send('service_id', 'template_id', templateParams, 'api_key').then(
-    //     (response) => {
-    //       console.log('SUCCESS!', response.status, response.text);
-    //     },
-    //     (error) => {
-    //       console.log('FAILED...', error);
-    //     },
-    //   );
-    //   setName('');
-    //   setEmail('');
-    //   setMessage('');
-    // }
+      console.log(templateParams);
+      emailjs.send("service_id", "template_id", templateParams, "api_key").then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
   };
 
   return (
@@ -54,15 +54,15 @@ function Contact() {
       <div className="items-container">
         <div className="contact_wrapper">
           <h1>Contact Me</h1>
-          <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
-          <Box
-            ref={form}
-            component="form"
+          <p>Interested in working with me? Let's collaborate.</p>
+          <form
+            ref={formRef}
             noValidate
             autoComplete="off"
-            className='contact-form'
+            className="contact-form"
+            onSubmit={sendEmail}
           >
-            <div className='form-flex'>
+            <Box className="form-flex">
               <TextField
                 required
                 id="outlined-required"
@@ -85,9 +85,11 @@ function Contact() {
                   setEmail(e.target.value);
                 }}
                 error={emailError}
-                helperText={emailError ? "Please enter your email or phone number" : ""}
+                helperText={
+                  emailError ? "Please enter your email or phone number" : ""
+                }
               />
-            </div>
+            </Box>
             <TextField
               required
               id="outlined-multiline-static"
@@ -103,10 +105,10 @@ function Contact() {
               error={messageError}
               helperText={messageError ? "Please enter the message" : ""}
             />
-            <Button variant="contained" endIcon={<SendIcon />} onClick={sendEmail}>
+            <Button type="submit" variant="contained" endIcon={<SendIcon />}>
               Send
             </Button>
-          </Box>
+          </form>
         </div>
       </div>
     </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,7 +4,10 @@ import mock02 from "../assets/images/mock02.png";
 import mock03 from "../assets/images/mock03.png";
 import mock04 from "../assets/images/mock04.png";
 import mock05 from "../assets/images/mock05.png";
-import mock06 from "../assets/images/mock06.png";
+
+import protfolio from "../assets/images/Portfolio.webp";
+import rental from "../assets/images/Rental.gif";
+
 import "../assets/styles/Project.scss";
 
 function Project() {
@@ -13,131 +16,64 @@ function Project() {
       <h1>Personal Projects</h1>
       <div className="projects-grid">
         <div className="project">
-          <a
-            href="https://react-side-project-rentals.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock06} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://react-side-project-rentals.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>House Rental</h2>
-          </a>
-          <p>
-            This mobile application allows realtors in Japan to securely manage
-            their property information and view future income predictions. This
-            app is built with Ruby on Rails and JavaScript.
-          </p>
+          <div>
+            <a
+              href="https://react-side-project-rentals.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={rental} className="zoom" alt="thumbnail" width="100%" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://react-side-project-rentals.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h2>Rental Web App</h2>
+            </a>
+            <p>
+              The AirBnB is a house rental platform built with Next.js,
+              Supabase, and REST APIs, offering a seamless experience for
+              renters and landlords. Users can easily browse detailed property
+              listings, apply filters, and securely manage bookings. Whether
+              you're searching for a cozy apartment or listing a home, our
+              platform simplifies the entire process.
+            </p>
+          </div>
         </div>
         <div className="project">
-          <a
-            href="https://www.byuh.edu/covid-19-case-management"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock05} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://www.byuh.edu/covid-19-case-management"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>COVID-19 Case Management</h2>
-          </a>
-          <p>
-            Built official charts for COVID/vaccination tracking for an
-            educational institution using JavaScript and the Google Sheets API
-            v4. The dashboard served the university's leadership in their
-            decision-making processes.
-          </p>
-        </div>
-        <div className="project">
-          <a
-            href="https://github.com/yujisatojr/multi-reg-analysis"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock04} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://github.com/yujisatojr/multi-reg-analysis"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>Multiple Regression Property Analysis</h2>
-          </a>
-          <p>
-            Analyzed the real estate market in Japan and predicted property
-            prices by implementing statistical methods such as OLS and
-            multi-regression analysis. This project leveraged Python and various
-            libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.
-          </p>
-        </div>
-        <div className="project">
-          <a
-            href="https://holokai.byuh.edu/programs-of-study"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock03} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://holokai.byuh.edu/programs-of-study"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>Programs of Study</h2>
-          </a>
-          <p>
-            Designed and developed a custom component for a CMS-based platform
-            (e.g., 'Brightspot') using Java, Handlebars, and LESS. University
-            students can find their majors of interest through this module.
-          </p>
-        </div>
-        <div className="project">
-          <a
-            href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock02} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>Transfer Evaluation Matrix</h2>
-          </a>
-          <p>
-            Created an interactive CSV table generator with Java, Handlebars,
-            and LESS. This project helps transfer students to quickly identify
-            eligible credits.
-          </p>
-        </div>
-        <div className="project">
-          <a
-            href="https://github.com/yujisatojr/submeowrine"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={mock01} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a
-            href="https://github.com/yujisatojr/submeowrine"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <h2>Submeowrine</h2>
-          </a>
-          <p>
-            Developed and released an Android mobile application using Java and
-            Android Studio that runs a 2D shooting game.
-          </p>
+          <div>
+            <a
+              href="https://www.byuh.edu/covid-19-case-management"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={protfolio}
+                className="zoom"
+                alt="thumbnail"
+                width="100%"
+              />
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="https://www.byuh.edu/covid-19-case-management"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <h2>COVID-19 Case Management</h2>
+            </a>
+            <p>
+              Built official charts for COVID/vaccination tracking for an
+              educational institution using JavaScript and the Google Sheets API
+              v4. The dashboard served the university's leadership in their
+              decision-making processes.
+            </p>
+          </div>
         </div>
       </div>
     </div>
